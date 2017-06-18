cb0799a837ded0ec419cca47c534cf9927f81ad5
Andrea Lorenzani <work@andrea.lorenzani.name>
2017-06-18 09:13:46 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "HAVacationRental"
+
+version := "1.0"
+
+scalaVersion := "2.12.2"
+    
---
+++ src/main/scala/name/andrea/lorenzani/homeaway/VacationRental.scala
@@ -0,0 +1,8 @@
+package name.andrea.lorenzani.homeaway
+
+/**
+  * Created by andrea on 18/06/17.
+  */
+class VacationRental extends App {
+
+}
